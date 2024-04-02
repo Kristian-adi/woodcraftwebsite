c2f102e18cb72abd2c78af8825234ebcd9fc75e1
Kristian Adi P <153663171+Kristian-adi@users.noreply.github.com>
2024-04-02 11:33:02 +0700

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ import Stats from "../components/Stats";
 const Hero = () => {
   const { title, subtitle, buttonText } = hero;
   return (
-    <section className="h-[850px] w-full bg-hero bg-right bg-cover bg-no-repeat text-white pt-[225px] pb-[254px] relative mb-12 lg:bg-center lg:mb-28">
+    <section id="hero" className="h-[850px] w-full bg-hero bg-right bg-cover bg-no-repeat text-white pt-[225px] pb-[254px] relative mb-12 lg:bg-center lg:mb-28">
       <div className="container mx-auto text-center">
         {/* title */}
         <h1 className="text-2xl mx-auto font-semibold mb-[30px] lg:text-[-64px] lg:leading-tight lg:max-w-[888px]">
