1977a3a4d2b7ab26afda97134109c94c861c9d64
Kristian Adi P <153663171+Kristian-adi@users.noreply.github.com>
2024-04-02 11:34:24 +0700

    Update Products.js

--- src/components/Products.js
+++ src/components/Products.js
@@ -7,7 +7,7 @@ import ProductSlider from "../components/ProductSlider";
 const Products = () => {
   const { title, subtitle } = products;
   return (
-    <section className="section text-center">
+    <section id="Features" className="section text-center">
       <div className="container mx-auto">
         <div>
           <h2 className="title">{title}</h2>
