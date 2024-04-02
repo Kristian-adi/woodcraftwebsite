ced1a329caa2856b2e5af12689ddb670dc069d98
Kristian Adi P <153663171+Kristian-adi@users.noreply.github.com>
2024-04-02 11:36:28 +0700

    Update Newsletter.js

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -5,7 +5,7 @@ import { newsletter } from "../data";
 const Newsletter = () => {
   const { title, subtitle, placeholder, buttonText } = newsletter;
   return (
-    <section className="section bg-newsletter min-h-[490px]">
+    <section id="contact" className="section bg-newsletter min-h-[490px]">
       <div className="container mx-auto flex justify-center lg:justify-end">
         <div className="w-full max-w-[558px] text-center text-white lg:text-left">
           <h2 className="text-4xl font-semibold max-w-[350px] mx-auto lg:mx-0 mb-5 leading-normal">
